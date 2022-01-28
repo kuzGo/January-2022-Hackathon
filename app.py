1ac28649b45cc785c93feafc4c62552479d29f99
marinaP <mar.pavlovic@gmail.com>
2022-01-28 22:51:21 +0100

    Add app.errorhandler(500)

--- app.py
+++ app.py
@@ -119,6 +119,11 @@ def timer():
 def page_not_found(e):
     return render_template("404.html"), 404
 
+# Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
 
 # export PRODUCTION=ON | OFF in TEST
 # PRODUCTION App -> Settings -> Reveal Config Vars -> KEY: PRODUCTION,
