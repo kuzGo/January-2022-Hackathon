568dcf80145c0bd4a07a507247922c4e5619321b
marinaP <mar.pavlovic@gmail.com>
2022-01-28 22:42:31 +0100

    app errorhandler(404)

--- app.py
+++ app.py
@@ -108,9 +108,16 @@ def home_page():
 
     return render_template("index.html")
 
+# Temporary route
 @app.route("/timer")
 def timer():
     return render_template("timer.html")
+
+# Create error pages
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 # export PRODUCTION=ON | OFF in TEST
